59f6de2e07e6a9765a6e82499055322b43648487
JoseArmandoSUP <124048843@upq.edu.mx>
2025-09-09 18:02:10 -0600

    Introducción a html y Java Script

---
+++ app.js
@@ -0,0 +1,25 @@
+document.write("<h1>Hola muendo</h1>");
+"Hola este es un string con comillas dobles"
+'Hola este es otro string pero con solo una comilla'
+
+1000000 // dato numerico	
+-2.3
+
+// datos booleanos
+true 
+false
+
+// arreglos
+['Billy', 'Gregory', 'Loois']
+[1, 2, 3]
+[true, false, true]
+
+// objeto
+{
+	username: 'Billy',
+	score: 70.4,
+	hours: 14, 
+	proffesional: true
+}
+
+console.log('Este es un string con console.log');
